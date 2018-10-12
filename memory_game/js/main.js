3c61869455d239b7bf0c23e1287aec56ff045341
Peter Chang <peterchang664@gmail.com>
2018-10-12 18:52:53 -0400

    update 10/12/18 WDI v.2

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+// alert("Up and running!");
+
+var card1= "king"
+var card2= "king"
+var card3= "queen"
+var card4= "queen"
+
+console.log("User flipped " + card1);
+console.log("User flipped " + card4);
