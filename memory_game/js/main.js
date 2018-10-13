b3c67f243eb9fe7b4b851eb397c5300972f94c40
Peter Chang <peterchang664@gmail.com>
2018-10-12 20:48:02 -0400

    update 10/12/18 WDI v.3

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,29 @@
 // alert("Up and running!");
 
-var card1= "king"
-var card2= "king"
-var card3= "queen"
-var card4= "queen"
+// var card1= "king"
+// var card2= "king"
+// var card3= "queen"
+// var card4= "queen"
 
-console.log("User flipped " + card1);
-console.log("User flipped " + card4);
+// console.log("User flipped " + card1);
+// console.log("User flipped " + card4);
+
+var cards = ["king","king","queen","queen"];
+var cardsInPlay = [];
+var card1 = cards[0];
+cardsInPlay.push(card1);
+console.log(cardsInPlay);
+
+var card2 = cards[1];
+cardsInPlay.push(card2);
+console.log(cardsInPlay);
+
+if (cardsInPlay.length === 2){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    }
+    else if(cardsInPlay[0] !== cardsInPlay[1]){
+        alert("Sorry try again.!");
+    }
+};
+
